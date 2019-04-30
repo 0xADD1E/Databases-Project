e11777418b309e1b0875f42b3913fe9bd5db3a1f
Devon Schimming <xnomadsix@gmail.com>
2019-04-30 09:37:25 -0500

    Printer didn't render accents right (#20)
    * Printer didn't render accents right
    
    jk its actually our fault but dont tell keenan
    
    * Frontend code

---
+++ Phase2/frontend/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+
+class Pokemon(models.Model):
+    pokedex_id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    gender_distribution = models.IntegerField()
+    legendary = models.BooleanField()
+
+
+class PokemonEvolution(models.Model):
+    base = models.ForeignKey(Pokemon,
+                             on_delete=models.CASCADE,
+                             related_name='evolves_to')
+    evolution = models.ForeignKey(Pokemon,
+                                  on_delete=models.CASCADE,
+                                  related_name='evolved_from')
+class Type(models.Model):
+    name = models.TextField(primary_key=True)
+
+class PokemonType(models.Model):
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE)
+    pokemon_type = models.ForeignKey(Type,
+                                     on_delete=models.CASCADE)
+
+class MIMEType(models.Model):
+    mime_string = models.TextField(primary_key=True)
+    friendly_name = models.TextField()
+    extension = models.TextField()
+
+
+class Media(models.Model):
+    # django auto-handles INT PK AI fields
+    filename = models.TextField()
+    data = models.BinaryField()
+    of = models.ForeignKey(Pokemon,
+                           on_delete=models.PROTECT)
+    mime = models.ForeignKey(MIMEType,
+                             on_delete=models.PROTECT)
+
+
+class Platform(models.Model):
+    name = models.TextField(primary_key=True)
+
+
+class Game(models.Model):
+    name = models.TextField(primary_key=True)
+    release_date = models.DateField()
+    platform = models.ForeignKey(Platform,
+                                 on_delete=models.PROTECT)
+
+
+class PokemonAppearance(models.Model):
+    base_hp = models.IntegerField()
+    base_speed = models.IntegerField()
+    base_attack = models.IntegerField()
+    base_defence = models.IntegerField()
+    base_sp_attack = models.IntegerField()
+    base_sp_defence = models.IntegerField()
+
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.PROTECT)
+    game = models.ForeignKey(Game,
+                             on_delete=models.PROTECT)
+
+
+class Attack(models.Model):
+    name = models.TextField(primary_key=True)
+    pp = models.IntegerField()
+    power = models.IntegerField()
+    priority = models.IntegerField()
+    accuracy = models.IntegerField()
+    tm_number = models.IntegerField(default=None, blank=True, null=True)
+
+
+class PokemonCanLearn(models.Model):
+    at_level = models.IntegerField()
+    appearance = models.ForeignKey(PokemonAppearance,
+                                   on_delete=models.CASCADE)
+    attack = models.ForeignKey(Attack,
+                               on_delete=models.CASCADE)
+
+class TypeMatchup(models.Model):
+    effectiveness_multiplier = models.FloatField()
+    this = models.ForeignKey(Type,
+                             on_delete=models.CASCADE,
+                             related_name='+')
+    other = models.ForeignKey(Type,
+                              on_delete=models.CASCADE,
+                              related_name='+')
+
+
+class AttackTypes(models.Model):
+    attack = models.ForeignKey(Attack,
+                               on_delete=models.CASCADE)
+    attack_type = models.ForeignKey(Type,
+                                    on_delete=models.CASCADE)
---
+++ Phase2/frontend/views.py
@@ -0,0 +1,77 @@
+import logging
+from django.shortcuts import render
+
+
+def pokemon_view(request):
+    from .models import Pokemon
+    from math import ceil
+    from base64 import b64encode
+    from collections import namedtuple
+    from django.db import connection
+    log = logging.getLogger('pokemon_view')
+    CARDS_PER_PAGE = 18
+
+    pagination_item = namedtuple('Page', ['number', 'display', 'is_active'])
+    page = int(request.GET.get('page', '0'))
+    total_pages = ceil(Pokemon.objects.count()/18)
+    display_pages = [pagination_item(number=n, display=n+1, is_active=(n == page))
+                     for n in range(total_pages)
+                     if abs(page-n) < 7]
+
+    pokemon = namedtuple('Pokemon', ['name', 'pokedex_id', 'image'])
+    pokemon_list = []
+    with connection.cursor() as cursor:
+        cursor.execute("""
+        SELECT frontend_pokemon.pokedex_id AS pokedex_id,
+            frontend_pokemon.name AS name,
+            first_media.data AS image,
+            first_media.mime_id AS mime
+        FROM frontend_pokemon
+        LEFT JOIN (
+            SELECT *
+            FROM frontend_media
+            GROUP BY frontend_media.of_id
+            ) AS first_media ON first_media.of_id = frontend_pokemon.pokedex_id
+        ORDER BY frontend_pokemon.pokedex_id
+        LIMIT %s OFFSET %s""", [CARDS_PER_PAGE, CARDS_PER_PAGE*page])
+        for (pokedex_id, name, image, mime) in cursor.fetchall():
+            image_encoded = "data:{};base64,{}".format(
+                mime,
+                b64encode(image).decode('UTF-8')
+            ) if image!=None else None
+            pokemon_list.append(pokemon(name=name,
+                                        pokedex_id=pokedex_id,
+                                        image=image_encoded))
+
+    return render(request, 'pokemon_list.html', {'pokemon_list': pokemon_list, 'pages': display_pages, 'max': total_pages-1})
+
+
+def kalos_uploader(request):
+    from .models import Pokemon, Media, MIMEType
+    import json
+    import requests
+    from django import forms
+    log = logging.getLogger('kalos_import')
+
+    class KalosForm(forms.Form):
+        kalos = forms.FileField()
+
+    if request.method == 'POST':
+        form = KalosForm(request.POST, request.FILES)
+        png = MIMEType.objects.get(mime_string='image/png')
+        for pkmn in json.load(request.FILES['kalos']):
+            new = Pokemon(pokedex_id=pkmn['number'],
+                          name=pkmn['name'],
+                          gender_distribution=0,
+                          legendary=False)
+            new.save()
+            log.info('Added pokemon {}'.format(new.name))
+            thumb = Media(filename='{}_thummbnail.png'.format(new.name),
+                          mime=png,
+                          data=requests.get(pkmn['ThumbnailImage']).content,
+                          of=new)
+            thumb.save()
+            log.debug('Saved thumbnail for {}'.format(new.name))
+    else:
+        form = KalosForm()
+    return render(request, 'upload.html', {'form': form, 'is_valid': form.is_valid()})
